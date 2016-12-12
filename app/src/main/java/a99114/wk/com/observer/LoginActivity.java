2fea6fac98bab18bb5e4bdce0e2139d0df53fb7d
zhaojipu <zhaojipu@99114.com>
2016-12-12 13:33:35 +0800

    登录界面创建

---
+++ app/src/main/java/a99114/wk/com/observer/LoginActivity.java
@@ -0,0 +1,16 @@
+package a99114.wk.com.observer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/12 0012.
+ */
+
+public class LoginActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
